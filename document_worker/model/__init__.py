ee98c7018c362d7da45527524db400c3a9ad232e
Marek Suchánek <marek.suchanek@gmail.com>
2021-01-11 16:54:27 +0100

    [WIP] Enhance filters

---
+++ document_worker/model/__init__.py
@@ -0,0 +1,5 @@
+
+# TODO: sanizitize replies (only valid current replies should be used)
+# TODO: suffix map
+# TODO: ectx|getReplyByPath(path)
+# TODO: ectx|getRepliesByUUID(uuid)
---
+++ document_worker/model/containers.py
@@ -0,0 +1,16 @@
+
+
+class SuffixReplyTree:
+
+    def __init__(self, replies, depth=0):
+        self.replies = replies
+        self.parents = self._construct_parents()
+
+    def _construct_parents(self):
+        parents = dict()
+
+        return parents
+
+
+class EnhancedDocumentContext:
+    pass
