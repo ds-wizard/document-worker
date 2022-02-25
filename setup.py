17f305c3f6df5c2fdf406c112e545fa8fce7b2eb
Marek Suchánek <marek.suchanek@gmail.com>
2022-02-25 07:39:20 +0100

    Release 3.9.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='document_worker',
-    version='3.8.0',
+    version='3.9.0',
     description='Worker for assembling and transforming documents',
     long_description=long_description,
     long_description_content_type='text/markdown',
